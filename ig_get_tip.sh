78137db06583a7b83ae81ecada9b8c53b220a074
zeliangz <zeliangz@cn.ibm.com>
2013-11-05 19:04:54 +0800

    2013-11-05

--- ig_get_tip.sh
+++ ig_get_tip.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
+
+#set -e
 shopt -s -o nounset
 
 readonly tip_dir="$HOME/.tips"
 cur_file=${tip_dir}
 
-cd ${tip_dir}
+while getopts :f:l: opt
+do
+  case $opt in
+    f)
+      cur_file="${tip_dir}/${OPTARG}"
+      ;;
+    l)
+      cur_file="${OPTARG}"
+      ;;
+    *)
+      cur_file=${tip_dir}
+      ;;
+esac
+done
+
+cd ${tip_dir} || {
+  echo "not found directory ${tip_dir}"
+  exit 1
+}
 
 cat_file()
 {
@@ -16,15 +36,20 @@ cat_file()
   files=($(ls -F ${cur_file}| grep -iv '^ig_.*'))
   file_count=${#files[@]}
   if [[ ${file_count} -eq 0 ]]; then
-    [ ${cur_file} == ${tip_dir} ] && echo "根目录\"${tip_dir}\"为空目录" && exit
-    [ ${cur_file} != ${tip_dir} ] && cur_file="${cur_file%/*/}" && cat_file && return
+    [ ${cur_file} == ${tip_dir} ] && {
+      echo "根目录\"${tip_dir}\"为空目录"
+      exit 1
+    } || {
+      cur_file="${cur_file%/*/}"
+      cat_file
+      return
+    }
   fi
 
   # 获取随机文件
   random_num=$((RANDOM%+${file_count}))
   random_file=${files[${random_num}]}
   cur_file="${cur_file}/${random_file}"
-  # echo $cur_file
 
   [ ! -d ${cur_file} ] && cat ${cur_file} || {
     cat_file
@@ -33,4 +58,5 @@ cat_file()
 }
 
 cat_file
+echo ${cur_file}
 
